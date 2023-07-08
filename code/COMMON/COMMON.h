181e4b1ab2588a5140d824124702866f95179308
Wsh <138436351+Wsh1273591351@users.noreply.github.com>
2023-07-09 04:19:45 +0800

    Add files via upload
    修复了由于本人的愚蠢粗心造成的一些bug，增加全局变量记录当前关卡数

--- code/COMMON/COMMON.h
+++ code/COMMON/COMMON.h
@@ -1,9 +1,19 @@
-#include "push_box_base.h"
-
 #ifndef _MAP_H_
 #define _MAP_H_
 
+#include "push_box_base.h"
+
 #define MAXN 20
+
+extern int current_map_count;
+
+enum directiom
+{
+	Up,
+	Down,
+	Left,
+	Right
+};
 
 struct position
 {
@@ -26,6 +36,9 @@ public:
 	Map(int bn, int w[MAXN][MAXN], position des[]) noexcept;
 	~Map() noexcept;
 	set<position> get_destination();
+	void set_box_number(int bx);
+	void set_wall(int w[MAXN][MAXN]);
+	void set_destination(std::set<position> des);
 	bool if_wall(int x, int y);
 };
 
@@ -39,7 +52,7 @@ public:
 	position get_position() const noexcept;
 	void set_position(position p);
 	//传入移动方向，判断能否进行移动（对应方向有墙不行，或有箱子且箱子后面还有东西不想）
-				void move(char c);
+				void move_player(char c);
 };
 
 class Box
@@ -52,9 +65,9 @@ public:
 	position get_position() const noexcept;
 	void set_position(position p);
 	//对应方向有墙返回1，没有返回0
-	bool check_around(char c) throw(int);
+	bool check_around_if_wall(char c) throw(int);
 	//'n'不移动，其他情况朝对应方向移动
-				void move(char c, Map map);
+				void move_box(char c, Map game_map);
 };
 
 #endif
