2412348590f3e9b263f19336a903f3c01ed9329e
enninnvxi <106723763+enninnvxi@users.noreply.github.com>
2023-07-09 16:00:55 +0800

    用了最新vm，关卡问题未完成

--- code/WINDOWS/Mainwindow.cpp
+++ code/WINDOWS/Mainwindow.cpp
@@ -2,25 +2,34 @@
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
-    , blockPixmap(":/image/block.png")
-    , boxPixmap(":/image/box.png")
-    , wallPixmap(":/image/wall.png")
-    , aimPixmap(":/image/ball.png")
-    , upPixmap(":/image/up.png")
-    , downPixmap(":/image/down.png")
-    , rightPixmap(":/image/right.png")
-    , leftPixmap(":/image/left.png")
+    , blockPixmap(":/img/block.png")
+    , boxPixmap(":/img/box.png")
+    , wallPixmap(":/img/wall.png")
+    , aimPixmap(":/img/ball.png")
+    , upPixmap(":/img/up.png")
+    , downPixmap(":./img/down.png")
+    , rightPixmap(":/img/right.png")
+    , leftPixmap(":/img/left.png")
 {
-    round = 1;
-    
 
+    scene->setItemIndexMethod(QGraphicsScene::NoIndex);
+    scene->setSceneRect(0, 0, 700, 700);
+    view->setFixedSize(700, 700);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setFrameStyle(0);
+    view->setSceneRect(0, 0, 700, 700);
+
+
+    round = 1;
+    step = 0;
     roundLabel = new QLabel(this);
     //roundLabel->setAlignment(Qt::AlignCenter);
-    
     stepLabel = new QLabel(this);
     
     this->setWindowTitle(title);
-    InitGame();
+    setDirection(Down);
+    LoadGame();
 
     nextBtn = new QPushButton(QString("下一关"), this);
     restartBtn = new QPushButton(QString("重新开始"), this);
@@ -54,50 +63,92 @@ MainWindow::MainWindow(QWidget* parent)
 
 }
 
-void MainWindow::InitGame() {
-    step = 0;
-    playerItem = new QGraphicsPixmapItem(downPixmap);
+
+void MainWindow::LoadGame() {
+    setviewMap();
+    int aimiter = 0; 
+    int boxiter = 0;
+    scene->clear();
     for (int i = 0; i < 20; i++)
     {
         for (int j = 0; j < 20; j++)
         {
             mapItem[i][j] = new QGraphicsPixmapItem(blockPixmap);
             mapItem[i][j]->setZValue(0);
-            // 设置图片项的位置
             mapItem[i][j]->setPos(j * 35, i * 35);
-            // 将图片项添加到场景中
             scene->addItem(mapItem[i][j]);
-            if (map[i][j] == 1)  // 该位置有墙
+            if (viewMap[i][j] & 1)  // 该位置有墙
             {
                 wallItem[i][j] = new QGraphicsPixmapItem(wallPixmap);
                 wallItem[i][j]->setZValue(1);
                 wallItem[i][j]->setPos(j * 35, i * 35);
                 scene->addItem(wallItem[i][j]);
             }
-
+            if (viewMap[i][j] & 2) {  // 该位置有目标
+                aimItem[aimiter] = new QGraphicsPixmapItem(aimPixmap);
+                aimItem[aimiter]->setZValue(1);
+				aimItem[aimiter]->setPos(j * 35, i * 35);
+				scene->addItem(aimItem[aimiter]);
+				aimiter++;
+            }
+            if (viewMap[i][j] & 4) {  // 该位置有box
+                boxItem[boxiter] = new QGraphicsPixmapItem(boxPixmap);
+                boxItem[boxiter]->setZValue(1);
+                boxItem[boxiter]->setPos(j * 35, i * 35);
+                scene->addItem(boxItem[boxiter]);
+                aimiter++;
+            }
+            if(viewMap[i][j] & 8) {  // 该位置有人
+				playerItem = new QGraphicsPixmapItem(playerPixmap);
+				playerItem->setZValue(1);
+				playerItem->setPos(j * 35, i * 35);
+				scene->addItem(playerItem);
+			}
         }
     }
-    // 设置人物的初始位置
-    playerItem->setPos(40, 40);
-    // 将人物添加到场景中
-    roundLabel->setText(QString("第1关"));
-    stepLabel->setText(QString("移动次数：0"));
-    scene->addItem(playerItem);
-    // 将视图设置为MainWindow的中心部件
+    roundLabel->setText(QString("第" + QString::number(round) + "关"));
+    stepLabel->setText(QString("移动次数："+step));
     repaint();
 }
 
 bool MainWindow::isSuccessful() {
-    
+    if (1) {
+        return true;
+    }
+	else {
+		return false;
+	}
 }
 
 void MainWindow::checkGame() {
-    repaint();
-    stepLabel->setText(QString("移动次数："+step));
     if (isSuccessful()) {
-        round++;
-        InitGame();
+        QMessageBox::information(this, tr("Congratulations"), tr("You have completed this level!"));
+        if(round<roundNum)
+            round++;
+        else
+            round = 1;
     }
+    LoadGame();
+}
+
+void MainWindow::setviewMap() {
+    int**wall = vm->get_wall();
+    for (int i = 0; i < mapSize; i++) {
+        for (int j = 0; j < mapSize; j++) {
+            viewMap[i][j] = wall[i][j];
+        }
+    }
+    std::set<Box> box = vm->get_all_box();
+    box_num = box.size();
+    for (auto it = box.begin(); it != box.end(); it++) {
+		viewMap[it->get_position().x][it->get_position().y] += 4;
+	}
+	std::set<position> aim = vm->get_destination();
+    for (auto it = aim.begin(); it != aim.end(); it++) {
+		viewMap[it->x][it->y] += 2;
+	}
+    Player player = vm->get_player();
+	viewMap[player.get_position().x][player.get_position().y] += 8;
 }
 
 MainWindow::~MainWindow() {
@@ -120,28 +171,44 @@ void MainWindow::keyPressEvent(QKeyEvent* e) {
     switch (e->key()) 
     {
     case Qt::Key_W:
-        game->moveUp();
+        if (vm->move_operation(Up))
+            step++;
+        setDirection(Up);
         break;
     case Qt::Key_A:
-        game->moveLeft();
+        if(vm->move_operation(Left))
+			step++;
+        setDirection(Left);
         break;
     case Qt::Key_S:
-        game->moveDown();
+        if (vm->move_operation(Down))
+            step++;
+        setDirection(Down);
         break;
     case Qt::Key_D:
-        game->moveRight();
+        if (vm->move_operation(Right))
+            step++;
+        setDirection(Right);
         break;
     case Qt::Key_Up:
-        game->moveUp();
+        if (vm->move_operation(Up))
+            step++;
+        setDirection(Up);
         break;
     case Qt::Key_Left:
-        game->moveLeft();
+        if (vm->move_operation(Left))
+            step++;
+        setDirection(Left);
         break;
     case Qt::Key_Down:
-        game->moveDown();
+        if (vm->move_operation(Down))
+            step++;
+        setDirection(Down);
         break;
     case Qt::Key_Right:
-        game->moveRight();
+        if (vm->move_operation(Right))
+            step++;
+        setDirection(Right);
         break;
     default:
         break;
@@ -151,17 +218,38 @@ void MainWindow::keyPressEvent(QKeyEvent* e) {
 }
 
 void MainWindow::onNextBtnClicked(){
-    round++;
-	InitGame();
+    if (round < roundNum)
+        round++;
+    else
+        round = 1;
+    setDirection();
+	LoadGame();
 }
 
 void MainWindow::onRestartBtnClicked() {
-    InitGame();
+    setDirection();
+    LoadGame();
 }
 
 
 void MainWindow::onPreBtnClicked() {
-    round--;
-    InitGame();
+    if (round > 1)
+        round --;
+    else
+        round = roundNum;
+    setDirection();
+    LoadGame();
 }
 
+void MainWindow::setDirection(direction d = Down) {
+    if(d == Up)
+		playerPixmap = upPixmap;
+	else if(d == Down)
+		playerPixmap = downPixmap;
+	else if(d == Left)
+		playerPixmap = leftPixmap;
+	else if(d == Right)
+		playerPixmap = rightPixmap;
+	else
+		playerPixmap = downPixmap;
+}
--- code/WINDOWS/Mainwindow.h
+++ code/WINDOWS/Mainwindow.h
@@ -3,6 +3,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "../VIEWMODEL/VIEWMODEL.h"
+#include "../COMMON/COMMON.h"
 
 #include <QtWidgets/QMainWindow>   // 主窗口类
 #include <QtWidgets/QWidget>
@@ -14,7 +16,6 @@
 #include <QtWidgets/QPushButton>  // 按钮类
 #include <QtWidgets/QLabel>  // 标签类
 #include <QtWidgets/QMessageBox>  // 消息框类
-#include <QtWidgets/QLabel>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QHBoxLayout>
 #include <QtGUI/QPainter>
@@ -23,10 +24,10 @@
 #include <string>
 #include <map>
 #include <set>
+#include <vector>
 
-const int mapSize = 20;  // 地图大小
-
-
+extern const int mapSize = 20;  // 地图大小
+extern const int roundNum = 5;  // 块大小
 
 class MainWindow : public QMainWindow
 {
@@ -45,21 +46,28 @@ public:
     void onNextBtnClicked();
     void onRestartBtnClicked();
     void onPreBtnClicked();
+    void setDirection(direction d = Down);
 
 private:
-    void InitGame();
+    void InitMap();
+    void LoadGame();
     bool isSuccessful();
     void checkGame();
+    void setviewMap();
+
     int round;
     int step;
     const QString title = "推箱子游戏";
+    ViewModel* vm;
+    int viewMap[mapSize][mapSize];
+    int box_num;
 
-    int map[mapSize][mapSize];
+
     QGraphicsPixmapItem* mapItem[20][20];
     QGraphicsPixmapItem* wallItem[20][20];
     QGraphicsPixmapItem* playerItem;
     QGraphicsPixmapItem* boxItem[10];
-    QGraphicsPixmapItem* finalItem[10];
+    QGraphicsPixmapItem* aimItem[10];
 
     QPixmap wallPixmap;
     QPixmap aimPixmap;
@@ -69,6 +77,7 @@ private:
     QPixmap downPixmap;
     QPixmap rightPixmap;
     QPixmap leftPixmap;
+    QPixmap playerPixmap;
 
     QLabel* roundLabel;
     QLabel* stepLabel;
