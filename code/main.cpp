f96930bc289754962fcebe277daceecc67f3e988
Medivh31 <93381138+Medivh31@users.noreply.github.com>
2023-07-10 16:37:48 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,18 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include "APP/App.h"
 int main(int argc, char *argv[])
 {
 #if defined(Q_OS_WIN)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+    
+    QGuiApplication a(argc, argv);
+    App app;
+    app.run();
+//    QQmlApplicationEngine engine;
+//    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+//    if (engine.rootObjects().isEmpty())
+ //       return -1;
 
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
-
-    return app.exec();
+    return a.exec();
 }
